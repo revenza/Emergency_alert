41f581087169b4d38b984bba504290c022bdb752
revenza <ucokfajar372@gmail.com>
2024-12-15 17:48:00 +0700

    design homepage

--- home.php
+++ home.php
@@ -14,12 +14,35 @@ if (!isset($_SESSION["id"]) || !isset($_SESSION["username"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Emergency Alert</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Righteous&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
 </head>
 
 <body>
+    <div class="sidenav">
+        <div class="sidenav-header">
+            <h2>EMERGENCY ALERT</h2>
+        </div>
 
-    <h1>HOME PAGeE</h1>
-    <a href="login/logout.php"><button>logout</button></a>
+        <nav>
+            <ul>
+                <li><a href=#>Dashboard</a></li>
+                <li><a href=#>Blog</a></li>
+                <li><a href="login/logout.php"><button>logout</button></a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="main">
+        <h1>HOME PAGeE</h1>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
 </body>
 
 </html>
